f0a757b406a7336dfd64b9d13c880c77965a5f11
bredsk <nicolas.programador@gmail.com>
2024-02-11 23:13:11 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+def mensajes():
+    print("--------------------------")
+    print("Hola mundo")
+    print("Serás un profesor de excelencia")
+    print("Te costo pero luchaste y ganaste la batalla")
+
+mensajes()
